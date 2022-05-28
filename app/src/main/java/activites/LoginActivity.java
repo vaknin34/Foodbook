81d63534983e3783af19beb3614c39b27bb6383d
niv vaknin <39972445+vaknin34@users.noreply.github.com>
2022-05-28 12:44:40 +0300

    Add Sign in

--- app/src/main/java/activites/LoginActivity.java
+++ app/src/main/java/activites/LoginActivity.java
@@ -13,7 +13,6 @@ import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -34,8 +33,6 @@ public class LoginActivity extends AppCompatActivity {
         signInLauncher.launch(signInIntent);
 
     }
-
-    // See: https://developer.android.com/training/basics/intents/result
     private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
             new FirebaseAuthUIActivityResultContract(),
             this::onSignInResult
@@ -46,18 +43,13 @@ public class LoginActivity extends AppCompatActivity {
         if (result.getResultCode() == RESULT_OK) {
             // Successfully signed in
             FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-            // ...
+            Intent intent = new Intent(this, HomePage.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+            intent.putExtra(getString(R.string.user), user);
+            startActivity(intent);
         } else {
-            // Sign in failed. If response is null the user canceled the
-            // sign-in flow using the back button. Otherwise check
-            // response.getError().getErrorCode() and handle the error.
-            // ...
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
         }
     }
-
-
-
-
 }
