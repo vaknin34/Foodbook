4ae1175a64e15959e484dfad511a5717d1e2334a
yuvalamir123 <yuval.amir998@gmail.com>
2022-06-27 20:24:21 +0300

    screen for the first time

--- app/src/main/java/com/example/foodbook/activities/LoginActivity.java
+++ app/src/main/java/com/example/foodbook/activities/LoginActivity.java
@@ -1,12 +1,15 @@
 package com.example.foodbook.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.foodbook.databinding.ActivityLoginBinding;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
@@ -14,12 +17,33 @@ public class LoginActivity extends AppCompatActivity {
     private ActivityLoginBinding binding;
     private String mail, password;
 
+    SharedPreferences loginPreference;
+    String MY_PREF = "my_pref";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        // initialize SharePreference
+        loginPreference = getSharedPreferences(MY_PREF, Context.MODE_PRIVATE);
+
+        // this condition will do the trick.
+        if(loginPreference.getString("tag", "notok").equals("notok")){
+
+            // add tag in SharedPreference here..
+            SharedPreferences.Editor edit = loginPreference.edit();
+            edit.putString("tag", "ok");
+            edit.commit();
+
+            new MaterialAlertDialogBuilder(this)
+                    .setMessage("Welcome To FoodBook!")
+                    .setPositiveButton("Continue", (dialogInterface, i) -> {})
+                    .show();
+
+        }
 
         firebaseAuth = FirebaseAuth.getInstance();
 
