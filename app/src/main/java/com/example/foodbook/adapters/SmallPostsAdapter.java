335b3ab65a09376919415e874fdaece23447a06d
yuvalamir123 <yuval.amir998@gmail.com>
2022-07-02 19:19:18 +0300

    fix also small post sort

--- app/src/main/java/com/example/foodbook/adapters/SmallPostsAdapter.java
+++ app/src/main/java/com/example/foodbook/adapters/SmallPostsAdapter.java
@@ -14,6 +14,10 @@ import com.example.foodbook.R;
 import com.example.foodbook.databases.FirebaseStorageManager;
 import com.example.foodbook.models.Post;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 import com.example.foodbook.interfaces.ItemClickInterface;
@@ -71,6 +75,16 @@ public class SmallPostsAdapter extends RecyclerView.Adapter<SmallPostsAdapter.Po
 
     public void setPosts(List<Post> s){
         posts = s;
+        Collections.sort(posts, (p1, p2) -> {
+            try {
+                Date date1 =new SimpleDateFormat("MM-dd HH:mm").parse(p1.getDate());
+                Date date2 =new SimpleDateFormat("MM-dd HH:mm").parse(p2.getDate());
+                return date2.compareTo(date1);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            return 0;
+        });
         notifyDataSetChanged();
     }
 
